255a8f0be1874a715e8519036a893571e418dc4c
hridaynath <hridaynathp@gmail.com>
2021-10-15 21:55:22 +0530

    Create complex.h

---
+++ complex.h
@@ -0,0 +1,61 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+#include<iostream>
+using namespace std;
+
+namespace complexnumber{
+class complex
+{
+    private:
+        double real;
+        double imaginary;
+
+    public:
+        complex(); // Default constructor
+        complex(double real, double imaginary); // Parameterized constructor
+
+        // Copy Constructor
+        complex(const complex&);
+
+        // Assignment opearator
+        complex& operator=(const complex&);
+
+        // Getter function for real component of complex number
+        double getReal() const;
+
+        // Getter function for imaginary component of complex number
+        double getImaginary() const;
+
+        // Setter function for real component of complex number
+        void setReal(double);
+
+        // Setter function for imaginary component of complex number
+        void setImaginary(double);
+
+        // Display function
+        void display();
+
+
+        //=======Left shift bit operator======//
+
+        /* This can be implemented as friend function or standalon function
+           Here we will implement standalone function */
+        friend ostream &operator<<(ostream&, const complex&);
+
+
+        //=======Plus + operator overloading======//
+
+        // Adds two complex numbers -> complex+complex
+        friend complex operator+(const complex &first, const complex &second);
+        // Adds complex and real number --> complex+real
+        friend complex operator+(const complex &first, const double real);
+        // Adds real and complex number --> real+complex
+        friend complex operator+(const double real, const complex &second);
+        // Destructor
+        virtual ~complex();
+};
+
+}
+
+#endif // COMPLEX_H
